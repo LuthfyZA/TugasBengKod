1cff2abd6aeb7a0d7cedc7fa6a6900a350fc99ed
Luthfy Z.A <lutfiangga7@gmail.com>
2025-04-16 21:40:45 +0700

    Add AdminLTE vendor files

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Models;  
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,6 +10,7 @@ class Obat extends Model
     use HasFactory;
     
     protected $table = 'obats';
+    
     protected $fillable = [
         'nama_obat',
         'kemasan',
